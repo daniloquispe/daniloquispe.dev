48ed31dbfb3c42b62ab89d2c96b4ccee771a550f
Danilo Quispe <dql@daniloquispe.dev>
2024-03-31 21:19:49 -0500

    Add navigation bar for inner pages
    This navbar has 2 behaviours:
    
    - For mobile devices, it just shows a "home" link
    - For desktop and other non-mobile devices, it shows a standard menu with the same 3 options from index page (about, resume and portfolio)

--- resources/views/layout-inner.blade.php
+++ resources/views/layout-inner.blade.php
@@ -2,7 +2,17 @@
 
 @section('content')
 	<header id="inner-header">
-		<a href="{{ route('index') }}"><img src="{{ asset('avatar.png') }}" alt="Avatar" /><h1>DaniloQuispe.dev</h1></a>
+		<div>
+			<a href="{{ route('index') }}"><img src="{{ asset('avatar.png') }}" alt="Avatar" /><h1>DaniloQuispe.dev</h1></a>
+		</div>
+		<nav class="main-menu">
+			<ul>
+				<li><a href="{{ route('index') }}">&larr; Home</a></li>
+				<li><a href="{{ route('about') }}" @if(Route::is('about')) class="active" @endif>About me</a></li>
+				<li><a href="https://resume.daniloquispe.dev" target="_blank">Resume</a></li>
+				<li><a href="{{ route('my-portfolio.index') }}" @if(Route::is('my-portfolio.*')) class="active" @endif>Portfolio</a></li>
+			</ul>
+		</nav>
 	</header>
 	<main id="inner-content">
 		@yield('content-inner')
