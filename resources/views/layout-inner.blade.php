c4d9217fb79d9be58edc45887a736870b3839dff
Danilo Quispe <dql@daniloquispe.dev>
2024-04-22 19:40:03 -0500

    Add social links
    Currently, GitHub and LinkedIn (as static links).

--- resources/views/layout-inner.blade.php
+++ resources/views/layout-inner.blade.php
@@ -18,6 +18,22 @@
 		@yield('content-inner')
 	</main>
 	<footer id="inner-footer">
+		<nav class="footer-menu">
+			<ul>
+				<li>
+					<a href="https://github.com/daniloquispe" target="_blank" title="GitHub">
+						<span class="icon-[ant-design--github-outlined]" role="img" aria-hidden="true"></span>
+						<span class="sr-only">GitHub</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://pe.linkedin.com/in/daniloquispe" target="_blank" title="LinkedIn">
+						<span class="icon-[ant-design--linkedin-outlined]" role="img" aria-hidden="true"></span>
+						<span class="sr-only">LinkedIn</span>
+					</a>
+				</li>
+			</ul>
+		</nav>
 		<p>&copy; 2024 Danilo Alejandro Quispe Lucana</p>
 	</footer>
 @endsection
